581679baf14a2c3b2a057b89673245d08d7c0f99
Natasha Fray <natasha.fray9@gmail.com>
2022-01-06 13:09:16 -0500

    changed copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return (
         <footer className="page-footer fixed-bottom w-100 m-0 p-0">
-            <span className="float-right text-dark px-1">&copy; Copyright 2021</span>
+            <span className="float-right text-dark px-1">&copy; Copyright 2022</span>
         </footer>
     )
 }
